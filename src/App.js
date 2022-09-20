9157845e03d65282f9054138b1b91da05a5b4d39
Office PC ABA (Fahad) <abasc1962@gmail.com>
2022-09-20 12:23:42 +0600

    Initial File Setupt & Component Function with props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+let name = "Fahad";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello, {name}</h1>
+      <Registration title="Full Stack Web Developer" name="Fuad"></Registration>
+    </>
+  );
+}
+
+function Registration(props) {
+  return (
+    <>
+      <h2>
+        I can Access the Variable from this Component as Well See: {name}
+      </h2>
+      <h1>We can access props: {props.title}</h1>
+      <h1>We can access props: {props.name}</h1>
+    </>
   );
 }
 
